61fa7c9560ad70a9253a4c15bae7fb7a88cba400
Michelle O'Connell <michelle.oco@gmail.com>
2025-07-19 20:18:19 -0600

    fix media queries, login modal

--- src/components/ProtectedRoutes/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import { ModalContext } from "../../contexts/ModalContext";
 import { Navigate } from "react-router-dom";
@@ -6,11 +6,17 @@ import { Navigate } from "react-router-dom";
 function ProtectedRoute({ children }) {
   const { isLoggedIn } = useContext(CurrentUserContext);
   const { openLoginModal } = useContext(ModalContext);
+  const wasLoggedInRef = useRef(isLoggedIn);
 
   useEffect(() => {
-    if (!isLoggedIn) {
+    // Only open login modal if user was never logged in when accessing this route
+    // Don't open it if they were logged in and then logged out (logout scenario)
+    if (!isLoggedIn && !wasLoggedInRef.current) {
       openLoginModal();
     }
+
+    // Update the ref to track the current login state
+    wasLoggedInRef.current = isLoggedIn;
   }, [isLoggedIn, openLoginModal]);
 
   return isLoggedIn ? children : <Navigate to="/" replace />;
